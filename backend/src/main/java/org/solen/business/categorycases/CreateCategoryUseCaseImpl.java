43541963cee61a3eadf43558e0798a7b62a826b1
Ivan Bodnar <i539830@fontysict.nl>
2026-05-21 16:38:05 +0200

    habit rebranding

--- backend/src/main/java/org/solen/business/categorycases/CreateCategoryUseCaseImpl.java
+++ backend/src/main/java/org/solen/business/categorycases/CreateCategoryUseCaseImpl.java
@@ -1,7 +1,7 @@
 package org.solen.business.categorycases;
 
 import org.solen.controller.dto.category.CreateCategoryRequest;
-import org.solen.domain.habits.Category;
+import org.solen.domain.practices.Category;
 import org.solen.business.repos.ICategoryRepository;
 import org.springframework.stereotype.Service;
 
