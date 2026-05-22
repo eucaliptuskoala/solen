bfc16a0b41828211becd365d69c614ac2f182a8b
Ivan Bodnar <i539830@fontysict.nl>
2026-05-22 13:15:33 +0200

    authentication fix

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -1,5 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import NavBar from './components/nav/NavBar'
+import AuthHandler from './apis/AuthHandler'
+
 import LandingPage from './pages/LandingPage'
 import DashboardPage from './pages/DashboardPage'
 import CheckInsPage from './pages/CheckInsPage'
@@ -8,16 +10,20 @@ import SignInPage from './pages/SignInPage'
 import InspirePage from './pages/InspirePage'
 import ProgressPage from './pages/ProgressPage'
 
+function RedirectIfAuthenticated({ children }) {
+  return AuthHandler.tokenExists() ? <Navigate to="/dashboard" replace /> : children
+}
+
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<RedirectIfAuthenticated><LandingPage /></RedirectIfAuthenticated>} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/checkins" element={<CheckInsPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<RedirectIfAuthenticated><SignUpPage /></RedirectIfAuthenticated>} />
+        <Route path="/sign-in" element={<RedirectIfAuthenticated><SignInPage /></RedirectIfAuthenticated>} />
         <Route path="/inspire" element={<InspirePage />} />
         <Route path="/progress" element={<ProgressPage />} />
       </Routes>
